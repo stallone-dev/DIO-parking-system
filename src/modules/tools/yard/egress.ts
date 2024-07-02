5d3c290b4ff39834d30104d64b8af137d22b7bb4
stallone-dev <stallone.developer@gmail.com>
2024-07-02 01:00:05 -0300

    Feat(project): Define main flows of yard control

---
+++ src/modules/tools/yard/egress.ts
@@ -0,0 +1,6 @@
+import { VehicleOnYard } from "../../models/vehicle-input-model.ts";
+
+export { egressOffYard };
+
+async function egressOffYard(vehicle: VehicleOnYard) {
+}
---
+++ src/modules/tools/yard/ingress.ts
@@ -0,0 +1,6 @@
+import { VehicleOnYard } from "../../models/vehicle-input-model.ts";
+
+export { ingressOnYard };
+
+async function ingressOnYard(vehicleData: VehicleOnYard) {
+}
