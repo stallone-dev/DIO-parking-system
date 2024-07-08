f5ec9c1c11b69d6f1729cc40e3a69422fbbf01d9
stallone-dev <stallone.developer@gmail.com>
2024-07-08 01:04:21 -0300

    perf(bundle): Update configurations of bundle controller

--- building.ts
+++ building.ts
@@ -3,6 +3,9 @@ import { build, stop } from "https://deno.land/x/esbuild@v0.21.5/mod.js";
 await build({
     entryPoints: ["src/modules/main.ts"],
     bundle: true,
+    platform: "browser",
+    minify: true,
+    treeShaking: true,
     format: "esm",
     outdir: "src/public/scripts",
     outExtension: { ".js": ".mjs" },
