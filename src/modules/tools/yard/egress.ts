bf32e57338f3bada93e748160afb52b6e4dc5a2e
stallone-dev <stallone.developer@gmail.com>
2024-07-08 00:57:23 -0300

    feat(yard): Define egress process for vehicles

--- src/modules/tools/yard/egress.ts
+++ src/modules/tools/yard/egress.ts
@@ -1,6 +1,34 @@
 import { VehicleOnYard } from "../../models/vehicle-input-model.ts";
+import { updateBackupOfYard } from "./backup.ts";
+import { recoveryBackupOfYard } from "./recovery.ts";
+import { updateUserYardList } from "./update.ts";
 
 export { egressOffYard };
 
-async function egressOffYard(vehicle: VehicleOnYard) {
+async function egressOffYard(element: HTMLTableRowElement) {
+    element?.addEventListener("click", function () {
+        removeVehicle(this.dataset["plate"]!);
+    });
+}
+
+async function removeVehicle(vehiclePlate: string) {
+    const vehicle_actual_list = await recoveryBackupOfYard();
+
+    const vehicle = vehicle_actual_list.find((vehicle) =>
+        vehicle.getPlate() === vehiclePlate
+    );
+    vehicle?.setSaidDate(new Date());
+
+    if (
+        !confirm(
+            `O veículo ${vehicle?.getName()} permaneceu por ${vehicle?.getYardTime()}\nDeseja encerrar?`,
+        )
+    ) return;
+
+    updateBackupOfYard(
+        vehicle_actual_list.filter((item) => item.getPlate() !== vehiclePlate),
+        true,
+    );
+
+    updateUserYardList();
 }
