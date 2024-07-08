14dbdd765fd0e7154396542c6b92e2c492d5a064
stallone-dev <stallone.developer@gmail.com>
2024-07-08 00:53:46 -0300

    feat(yard): Define ingress process for Vehicles

--- src/modules/tools/yard/ingress.ts
+++ src/modules/tools/yard/ingress.ts
@@ -1,6 +1,35 @@
 import { VehicleOnYard } from "../../models/vehicle-input-model.ts";
+import { $ } from "../query.ts";
+import { updateBackupOfYard } from "./backup.ts";
+import { recoveryBackupOfYard } from "./recovery.ts";
 
 export { ingressOnYard };
 
-async function ingressOnYard(vehicleData: VehicleOnYard) {
+async function ingressOnYard(
+    vehicle: VehicleOnYard,
+    backup?: boolean,
+): Promise<void> {
+    const yard_row = await createYardRow(vehicle);
+
+    $("#vehicle-yard")?.appendChild(yard_row);
+
+    updateBackupOfYard([...await recoveryBackupOfYard(), vehicle], backup);
+}
+
+async function createYardRow(
+    vehicle: VehicleOnYard,
+): Promise<HTMLTableRowElement> {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${vehicle.getName()}</td>
+        <td>${vehicle.getPlate()}</td>
+        <td>${new Date(vehicle.getEntryDate()).toLocaleString()}</td>
+        <td>
+            <button class="egress" data-plate="${vehicle.getPlate()}">X</button>
+        </td>
+    `;
+
+
+    return row;
 }
